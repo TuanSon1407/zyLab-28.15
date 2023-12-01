a2246e2781d9185e5d37d4eb15d785ed6fe38a17
TuanSon1407 <152203395+TuanSon1407@users.noreply.github.com>
2023-12-01 10:13:51 -0600

    Create main.cpp
    The default template of main.cpp from Zybook.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "PlaylistNode.h"
+
+using namespace std;
+
+void PrintMenu(const string playlistTitle) {
+   /* Type your code here */
+   
+}
+
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+   
+   return 0;
+}
