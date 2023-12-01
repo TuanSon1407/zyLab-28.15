a50562acd455d17a68cfc7a384ad8d4896d140ec
TuanSon1407 <152203395+TuanSon1407@users.noreply.github.com>
2023-12-01 10:33:42 -0600

    Update PlaylistNode.h
    Building the PlaylistNode.h following the specification mentioned in step 1 of Zybook.

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,1 +1,21 @@
+#ifndef PLAYLISTNODE_H
+#define PLAYLISTNODE_H
 
+class PlaylistNode {
+  public:
+    string GetID();
+    string GetSongName();
+    string GetArtistName();
+    int GetSongLength();
+    PlayListNode* GetNext();
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+    void PrintPlaylistNode();
+  private:
+    string uniqueID = "none";
+    string songName = "none";
+    string artistName = "none";
+    int songLength = 0;
+    PlaylistNode* nextNodePtr = nullptr;
+};
+#endif
