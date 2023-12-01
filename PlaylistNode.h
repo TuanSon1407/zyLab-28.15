176029f1a15ef09d6d3784cdb2977267f6cc2525
TuanSon1407 <152203395+TuanSon1407@users.noreply.github.com>
2023-12-01 11:07:52 -0600

    Update PlaylistNode.h
    Fixed the string problem in PlaylistNode.h

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,21 +1,24 @@
 #ifndef PLAYLISTNODE_H
 #define PLAYLISTNODE_H
 
+#include <string>
+
 class PlaylistNode {
   public:
-    PlaylistNode(string ID, string songTitle, string artist, int songDuration);
-    string GetID();
-    string GetSongName();
-    string GetArtistName();
+    PlaylistNode(std::string ID, std::string songTitle, std::string artist, int songDuration);
+    std::string GetID();
+    std::string GetSongName();
+    std::string GetArtistName();
     int GetSongLength();
     PlayListNode* GetNext();
     void InsertAfter(PlaylistNode* nodePtr);
     void SetNext(PlaylistNode* nodePtr);
     void PrintPlaylistNode();
+
   private:
-    string uniqueID = "none";
-    string songName = "none";
-    string artistName = "none";
+    std::string uniqueID = "none";
+    std::string songName = "none";
+    std::string artistName = "none";
     int songLength = 0;
     PlaylistNode* nextNodePtr = nullptr;
 };
